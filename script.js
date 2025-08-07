f45a2c6f5d6e1a80a688707b1974e815760ddf36
Roberto Holanda <0robertoholandadasilva.filho@gmail.com>
2025-08-07 11:53:05 -0300

    Removendo alguns comentários desnecessários e adicionando mais um elemento no efeito de redirecionamento na página

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 
 /*
     Apenas uma simulação do efeito scroll-behavior: smooth; do CSS3 em Javascript. 
-    Primeira Função => Executa o script após o carregamento da página. 
-    Segunda Função => Adiciona o efeito suave de scroll no elemento de id = "redirection" do HTML5.
-    Terceira Função => Insere o ano atual no <footer> da página. 
 */
 
 document.addEventListener("DOMContentLoaded", function() {
     const redirection = document.getElementById("redirection");
+    const redirection_url = document.getElementById("redirection-url");
     if (redirection) {
         redirection.addEventListener("click", function(event) {
             event.preventDefault();
             scrollToElement("part");
+        });
+    }
+
+    if (redirection_url){
+        redirection_url.addEventListener("click", function(event) {
+            event.preventDefault();
+            scrollToElement("url");
         });
     }
 });
@@ -27,10 +32,9 @@ function scrollToElement(elementId) {
     }
 }
 
-const set_year = (y) => {
+const set_year = () => {
+    const get_year = document.getElementById("year");
     let year_now = new Date().getFullYear();
-    return y.prepend(year_now)
+    return get_year.prepend(year_now)
 }
-
-const get_year = document.getElementById("year");
-set_year(get_year)
+set_year();
